96217b3c9c0a90babbde78c48f7939e561561443
Johnhu98 <Johnhu98@163.com>
2019-10-01 19:02:17 +0800

    change

---
+++ Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/3f5ff03fa0c024b930f515e63ae2c702.jpg_945x288_7dff4510.jpg'
+      }, {
+        id: '002',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/b59b8db9f72eb8f54df5bf2155672e9c.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+  .wrapper
+    overflow hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    .swiper-pagination-bullets-actives
+    .swiper-img
+      width: 100%
+</style>
